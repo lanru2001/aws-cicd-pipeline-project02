46dfd3ef1f2dc66e6f532bd10f508b71bf55efd2
Azeez Temitope Olanrewaju <59709429+lanru2001@users.noreply.github.com>
2021-03-09 01:24:59 -0600

    Update docker_script.sh

--- docker_script.sh
+++ docker_script.sh
@@ -1,21 +1,25 @@
 #!/bin/bash
 # simple script to install Docker on RedHat Linux/CentOS
+
+#Remove Podman container 
 dnf remove -y podman buildah
 
+#Install yum-utils package 
 dnf install -y yum-utils
-
-dnf install -y yum-utils
+dnf install -y yum-utils 
 
 yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
 
+#Install the official latest community edition
 dnf install -y docker-ce
 
 sudo yum makecache
 
 sudo dnf -y  install docker-ce --nobest
 
-sudo systemctl enable --now docker
+#Enable and start Docker Daemon
+sudo systemctl start  docker
+sudo systemctl enable docker
 
-systemctl status  docker
-
+#Run docker with privilege without a sudo 
 sudo usermod -aG docker $USER
