036da16b68187e870439ed914017b44cceef2e18
Azeez Temitope Olanrewaju <59709429+lanru2001@users.noreply.github.com>
2021-03-10 21:22:31 -0600

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -1,34 +1,11 @@
 resource "aws_s3_bucket" "app_web" {
-  bucket = "${var.account_name}-app_web"
+  bucket = "${var.account_name}-app-web"
   acl    = "private"     
-  versioning = {
+  versioning {
     enabled = true 
   }
   tags = {
-    Name        = "app_web"
+    Name        = "app-web"
     Environment = "Dev"
   }  
-}                                      
-
-resource "aws_s3_bucket_policy" "app_web" {
-  bucket = "${aws_s3_bucket.app_web.id}"
-  policy =<<POLICY
-{
-  "Id": "Policy1513880777555",
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Sid": "Stmt1513880773845",
-      "Action": "s3:*",
-      "Effect": "Allow",
-      "Resource": "${aws_s3_bucket.app_web.arn}",
-      "Principal": {
-        "AWS": [
-          "arn:aws:iam::${var.account_id}:role/${var.app}-app_web",
-        ]
-      }
-    }
-  ]
-}
-POLICY
-}
+} 
