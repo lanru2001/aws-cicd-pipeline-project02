3148d54c4bca60d7f655900a5f49c3230e2d24ef
Azeez Temitope Olanrewaju <59709429+lanru2001@users.noreply.github.com>
2021-03-13 04:00:35 -0600

    Update userdata.sh

--- userdata.sh
+++ userdata.sh
@@ -1,8 +1,21 @@
 #!/bin/bash
 yum update -y
 yum install nginx -y
-cp /tmp/index.html /var/www/html/index.html
-chmod 755 /var/www/index.html
+#cp /tmp/index.html /var/www/html/index.html
+#chmod 755 /var/www/index.html
 service nginx restart
 sudo firewall-cmd --permanent --zone=public --add-service=http
 sudo firewall-cmd --reload
+
+# install codedeploy agent on instance
+yum install ruby -y
+
+cd /tmp
+
+wget https://aws-codedeploy-us-east-2.s3.amazonaws.com/latest/install;
+
+chmod +x ./install
+
+./install auto
+
+service codedeploy-agent start
