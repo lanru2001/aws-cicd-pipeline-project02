454ce9f71cef5fd5cfa617e3c202593763f5ff7a
Azeez Temitope Olanrewaju <59709429+lanru2001@users.noreply.github.com>
2021-03-15 23:39:16 -0500

    Update codebuild.tf

--- codebuild.tf
+++ codebuild.tf
@@ -5,11 +5,12 @@ resource "aws_codebuild_project" "project" {
   service_role  = aws_iam_role.build_role.arn
 
   environment {
-    compute_type = "BUILD_GENERAL1_SMALL"
-    image        = var.docker_build_image
-    type         = "LINUX_CONTAINER"
-  }
+    compute_type                = "BUILD_GENERAL1_SMALL"
+    image                       = var.docker_build_image
+    type                        = "LINUX_CONTAINER"
+    image_pull_credentials_type = "CODEBUILD"
 
+  }
   source {
     type            = "GITHUB"
     location        = "https://github.com/lanru2001/aws-cicd-pipeline-project02.git"
@@ -30,7 +31,7 @@ resource "aws_codebuild_project" "project" {
   artifacts {
     type     = "S3"
     location = aws_s3_bucket.app_web.bucket
- 
+
   }
 
   cache {
@@ -45,3 +46,4 @@ resource "aws_codebuild_project" "project" {
   }
 
 }
+
