1e7d08c9c5758e2fe64942e78fecdbf8222fc89d
Azeez Temitope Olanrewaju <59709429+lanru2001@users.noreply.github.com>
2021-03-18 11:10:47 -0500

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -1,11 +1,24 @@
 resource "aws_s3_bucket" "app_web" {
-  bucket = "${var.account_name}-app-web"
-  acl    = "private"     
+  bucket = "${var.account_name}-application-web"
+  acl    = "private"
   versioning {
-    enabled = true 
+    enabled = true
   }
   tags = {
-    Name        = "app-web"
+    Name        = "application-web"
     Environment = "Dev"
-  }  
-} 
+  }
+}
+
+
+resource "aws_s3_bucket" "alb" {
+  bucket = "${var.account_name}-alb"
+  acl    = "private"
+  versioning {
+    enabled = true
+  }
+  tags = {
+    Name        = "alb"
+    Environment = "Dev"
+  }
+}
